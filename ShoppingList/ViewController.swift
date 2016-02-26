9406fb67a56cced195716e02316fe1be4cc93c9e
student <student@students-mac.student.georgianc.on.ca>
2016-02-26 11:32:18 -0500

    Final Commit - save/cancel constraints need finishing. List item label is reducing it's size to 1 character width. First stepper crashes app.

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var CancelButton: UIButton!
 
     // steppers
+
     @IBOutlet weak var stepper1: UIStepper!
     @IBOutlet weak var stepper2: UIStepper!
     @IBOutlet weak var stepper3: UIStepper!
@@ -23,32 +24,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var stepper5: UIStepper!
     
     // labels
+
     @IBOutlet weak var label1: UITextField!
     @IBOutlet weak var label2: UITextField!
     @IBOutlet weak var label3: UITextField!
     @IBOutlet weak var label4: UITextField!
     @IBOutlet weak var label5: UITextField!
     
-
+    // list numbers
+    @IBOutlet weak var num1: UILabel!
+    @IBOutlet weak var num2: UILabel!
+    @IBOutlet weak var num3: UILabel!
+    @IBOutlet weak var num4: UILabel!
+    @IBOutlet weak var num5: UILabel!
     
-    
+    // stepper functions
+    // when stepper button is pressed the number label changes to the value taken from the stepper
     @IBAction func step1(sender: UIStepper) {
-        label1.text = "\(Int(stepper1.value))"
+        num1.text = "\(Int(stepper1.value))"
     }
 
     @IBAction func step2(sender: UIStepper) {
-        label2.text = "\(Int(stepper2.value))"
+        num2.text = "\(Int(stepper2.value))"
     }
     
     @IBAction func step3(sender: UIStepper) {
-        label3.text = "\(Int(stepper3.value))"
+        num3.text = "\(Int(stepper3.value))"
     }
     @IBAction func step4(sender: UIStepper) {
-        label4.text = "\(Int(stepper4.value))"
+        num4.text = "\(Int(stepper4.value))"
     }
     @IBAction func step5(sender: UIStepper) {
-        label5.text = "\(Int(stepper5.value))"
+        num5.text = "\(Int(stepper5.value))"
     }
     
+    
+    // cancel button pressed
+    // changes all values back to default (0 or empty)
+    @IBAction func CancelButtonPress(sender: UIButton) {
+        num1.text = "0"
+        num2.text = "0"
+        num3.text = "0"
+        num4.text = "0"
+        num5.text = "0"
+        
+        label1.text = ""
+        label2.text = ""
+        label3.text = ""
+        label4.text = ""
+        label5.text = ""
+    }
 }
 
